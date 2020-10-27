d6585358810d866200df20174a43cbea62ac8c04
SIDDHARTHA PATKI <siddharthapatki@gmail.com>
2020-10-27 17:43:23 +0530

    added tap to start for mobile

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$(document).on('click keypress',function() {
   if (!started) {
     nextSequence();
     started = true;
